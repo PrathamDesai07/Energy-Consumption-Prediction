6b62e56eaf239a560c0a9623e81da50f8f273a1e
PrathamDesai07 <prathamdesai071204@gmail.com>
2025-04-06 14:08:33 +0000

    6. Model Training component done

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -18,3 +18,13 @@ class DataTransformationArtifact:
     transformed_object_file_path:str 
     transformed_train_file_path:str
     transformed_test_file_path:str
+
+@dataclass
+class ClassificationMetricArtifact:
+    MSE:float
+    MAE:float
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -38,3 +38,17 @@ class DataTransformationConfig:
     transformed_object_file_path: str = os.path.join(data_transformation_dir,
                                                      DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
                                                      PREPROCSSING_OBJECT_FILE_NAME)
+
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir: str = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR_NAME)
+    trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
+    expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
+    model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
+    _n_estimators = MODEL_TRAINER_N_ESTIMATORS
+    _learning_rate = MODEL_TRAINER_LEARNING_RATE
+    _sub_samples = MODEL_TRAINER_SUBSAMPLE
+    _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
+    _booster = MIN_SAMPLES_BOOSTER
+    _objective = MIN_SAMPLES_OBJECTIVE
+    _min_child_weight = MIN_SAMPLES_MIN_CHILD_WEIGHT
