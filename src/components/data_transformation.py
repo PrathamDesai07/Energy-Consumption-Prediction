7bb87a76acdfe5a0f8ee5b866f29c8a399a2442f
PrathamDesai07 <prathamdesai071204@gmail.com>
2025-04-07 06:46:13 +0000

    9. Flask app running locally

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -228,7 +228,7 @@ class DataTransformation:
             test_arr = np.c_[input_feature_test_arr, target_feature_test_arr]
             logging.info("feature-target concatenation done for train-test df.")
 
-            save_object(self.data_transformation_config.transformed_object_file_path, preprocessor)
+            save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_target)
             save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
             logging.info("Saving transformation object and transformed files.")
