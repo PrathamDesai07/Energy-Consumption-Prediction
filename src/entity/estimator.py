03f30d47cc220c5f72f385802cdf24786d18fb5d
PrathamDesai07 <prathamdesai071204@gmail.com>
2025-04-12 11:36:52 +0000

    14. preprocessor path changed

--- src/entity/estimator.py
+++ src/entity/estimator.py
@@ -36,7 +36,7 @@ class MyModel:
         try:
             logging.info("Starting prediction process.")
 
-            pipeline = joblib.load("Energy-Consumption-Prediction/preprocessing_target.pkl")
+            pipeline = joblib.load("./preprocessing_target.pkl")
 
             # Access the ColumnTransformer
             column_transformer = pipeline.named_steps['Preprocessor']
